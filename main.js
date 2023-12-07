8fc1d28c03831d8666974aad7bdaf5783d7fc5c5
Raymundo Lara <Lara9632@LAPTOP-5MMEC2QJ>
2023-12-07 17:21:08 -0600

    Markers created and working succesfully, pending to refactor, style delete and connet to API

--- main.js
+++ main.js
@@ -1,99 +1,116 @@
-import { apiResponse } from './apiResponse';
-import './style.css'
-import { Loader } from "@googlemaps/js-api-loader";
-import axios from 'axios';
+let map;
+let marker;
+let results;
 
-
-async function initMap() {
-  // Request needed libraries.
+async function initMap({ lat, lng, zoom }) {
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const map = new Map(document.getElementById("map"), {
-    center: { lat: parseFloat(apiResponse.parameters.lat), lng: parseFloat(apiResponse.parameters.lng) },
-    zoom: parseInt(apiResponse.parameters.zoom),
+  map = new Map(document.getElementById("map"), {
+    center: { lat: parseFloat(lat), lng: parseFloat(lng) },
+    zoom: parseInt(zoom),
     mapId: "4504f8b37365c3d0",
     disableDefaultUI: true,
   });
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  marker = AdvancedMarkerElement;
+}
 
-  function createMarker({ lat, lng, number }) {
+function createMarker({ lat, lng, number, marker }) {
 
-    const circle = document.createElement("div");
-
-    circle.className = "circle";
+  const circle = document.createElement("div");
+  circle.textContent = 'NaN';
+  circle.className = "circle";
+  if (number > 0) {
+    circle.style.backgroundColor = '#189b25';
+    if (number > 3 && number < 10) circle.style.backgroundColor = '#da640b';
+    if (number >= 10) circle.style.backgroundColor = '#b8000a';
     circle.textContent = number;
-
-    const marker = new AdvancedMarkerElement({
-      map,
-      position: { lat, lng },
-      content: circle,
-    });
-
-    return marker;
   }
+  const createMarker = new marker({
+    map,
+    position: { lat, lng },
+    content: circle,
+  });
+}
 
+// async function getData({ keyword, nearBy, businessId }) {
+//   const options = {
+//     method: 'GET',
+//     url: 'https://local-rank-tracker.p.rapidapi.com/grid',
+//     params: {
+//       place_id: 'ChIJoejvAr3Mj4ARtHrbKxtAHXI',
+//       query: keyword,
+//       lat: '37.341759',
+//       lng: '-121.938314',
+//       grid_size: '5',
+//       radius: '1',
+//       zoom: '13'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'cb591c92demsh9ce0726c6e22684p1e6efcjsn6b412bd81878',
+//       'X-RapidAPI-Host': 'local-rank-tracker.p.rapidapi.com'
+//     }
+//   };
+//   try {
+//     const response = await axios.request(options);
+//     console.log(response);
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-  const query = document.getElementById("submit");
-
-  query.addEventListener('click', (e) => {
-    let businessId = document.getElementById('businessId').value;
-    let keyword = document.getElementById('keyword').value;
-    let nearBy = document.getElementById('near').value;
-
-    const options = {
-      method: 'GET',
-      url: 'https://local-rank-tracker.p.rapidapi.com/places',
-      params: {
-        query: keyword,
-        near: nearBy,
-      },
-      headers: {
-        'X-RapidAPI-Key': '4b2dbca1d5msh9865f07a7ca1fd1p1f5599jsna29ef4e76b08',
-        'X-RapidAPI-Host': 'local-rank-tracker.p.rapidapi.com'
-      }
-    };
-
-    try {
-      const response = axios.request(options);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
+function fetchData({ urlApi, query, placeId, lat, lng, grid, zoom }) {
+  const url = `${urlApi}?place_id=${placeId}&query=${query}&lat=${lat}&lng=${lng}&grid_size=${grid}&radius=1&zoom=${zoom}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+      'X-RapidAPI-Host': 'local-rank-tracker.p.rapidapi.com'
     }
+  };
+  return fetch(url, options)
+}
 
-    if (initMap && results) {
+const button = document.getElementById("submit");
+
+const API = 'https://local-rank-tracker.p.rapidapi.com/grid'
+
+button.addEventListener('click', (e) => {
+  let businessId = document.getElementById('businessId').value;
+  let keyword = document.getElementById('keyword').value;
+  let grid = document.getElementById('grid').value;
+  let lat = document.getElementById('lat').value;
+  let lng = document.getElementById('lng').value;
+  let zoom = document.getElementById('zoom').value;
+  button.textContent = 'Generating markers...';
+  fetchData({
+    urlApi: API,
+    query: keyword,
+    placeId: businessId,
+    lat,
+    lng,
+    zoom,
+    grid,
+  })
+    .then(response => response.json())
+    .then(response => {
+      results = response.data.results;
       results.forEach(result => {
         createMarker({
           lat: parseFloat(result.lat),
           lng: parseFloat(result.lng),
-          number: parseInt(result.rank)
+          number: parseInt(result.rank),
+          marker: marker,
         })
-      });
+      })
+      button.textContent = 'Markers generated'
+    })
+    .catch(err => console.error(err))
 
+  initMap({
+    lat,
+    lng,
+    zoom,
+  });
 
-
-    } else {
-      return
-    }
-  })
-
-}
-
-let results = apiResponse.data.results;
-console.log(results);
-
-// https://rapidapi.com/letscrape-6bRBa3QguO5/api/local-rank-tracker
-
-
-
-initMap();
-
-// Configurar la solicitud fetch
-
-//   method: 'GET',
-//   headers: {
-//     'Content-Type': 'application/json'
-//     // Puedes agregar más encabezados según sea necesario
-//   },
-//   body: JSON.stringify(data)
-// })
-//   
-
+})
